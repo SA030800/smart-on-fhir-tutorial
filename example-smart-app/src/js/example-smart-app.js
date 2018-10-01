332121ba07379753907a0b69068ed1a77a35c869
SA030800 <Seada.ali@cerner.com>
2018-10-01 11:48:44 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -8,16 +8,16 @@
     }
 
     function onReady(smart)  {
+      window.alert("Test loading");
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
+                      _count: 2,
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        $or: ['http://loinc.org|8462-4']
                       }
                     }
                   });
